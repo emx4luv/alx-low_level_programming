f3b72e93ce9e6be94409735e4bfd8fb8b428435f
Emmanuel Enahoro <emmanuel.enahoro11@gmail.com>
2022-07-01 22:00:15 -0700

    if else commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/* printing the alphabet */
+
+int main(void)
+
+{
+
+		char c;
+
+
+
+			for (c = 'a'; c <= 'z'; c++)
+
+						putchar(c);
+
+				for (c = 'A'; c <= 'Z'; c++)
+
+							putchar(c);
+
+						putchar('\n');
+
+
+
+							return (0);
+
+}
