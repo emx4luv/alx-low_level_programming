693a7fa18d1a842056eabe884aa912e6ab778700
Emmanuel Enahoro <emx4luv.11@gmail.com>
2022-09-30 14:31:40 -0700

    103-keygen fix

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
- *
- * Return: 0 on success, 1 on error
- */
+* main - generate a key depending on a username for crackme5
+* @argc: number of arguments passed
+* @argv: arguments passed to main
+* Return: 0 on success, 1 on error
+*/
 int main(int argc, char *argv[])
 {
 	unsigned int i, b;
